1c5ec4a4a8a23eaed9e16cce608ac6382eec9a2f
Code2Prod <office@code2prog.com>
2021-01-12 22:14:43 +0700

    last update :)

--- wht_client.php
+++ wht_client.php
@@ -6,7 +6,7 @@ defined('ABSPATH') or die('No script kiddies please!');
  * Plugin URI: https://github.com/WhatArmy/WatchtowerWpClient
  * Description: The WhatArmy WordPress plugin allows us to monitor, backup, upgrade, and manage your site!
  * Author: WhatArmy
- * Version: 3.2.6
+ * Version: 3.2.7
  * Author URI: https://whatarmy.com
  **/
 
@@ -60,5 +60,30 @@ if (version_compare(PHP_VERSION, WHT_MIN_PHP) >= 0) {
 
     add_action('admin_notices', 'wht_admin_notice__error');
 }
+
+function wht_admin_notice__deprecation()
+{
+    ?>
+    <div class="error"
+         style="padding-top:15px;padding-bottom:15px;display:flex;flex-direction:row">
+        <div>
+            <img src="<?php echo WHT_MAIN_URI.'assets/images/logo1x.png'; ?>"
+                 style="height:100px;padding-right:15px;" alt="">
+        </div>
+        <div>
+            <h1>We’ve moved our plugin codebase to <a href="https://wordpress.org/plugin/watchtowerhq">wordpress.org</a>. <br>
+                Use the link below to install the updated plugin (3.3.X).<br>
+                Plugin version 3.2.X is no longer supported, please deactivate and uninstall.
+            </h1>
+            <h4 style="margin-bottom:0;">
+                <a href="/wp-admin/plugin-install.php?s=Watchtowerhq&tab=search&type=term">Click Here</a> to install new
+                version from wordpress.org.
+            </h4>
+        </div>
+    </div>
+    <?php
+}
+
+add_action('admin_notices', 'wht_admin_notice__deprecation');
 
 
